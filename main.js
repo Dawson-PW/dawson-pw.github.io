af83c4ecfad3eb347517b8ea9149d064d59af76a
Dawson <dawson.panterwray07@gmail.com>
2022-01-10 20:59:13 +0000

    REMOVE ALL SW

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-//service worker setup
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-      });
-    });
-}
 
 /////////////////////////////////
 
